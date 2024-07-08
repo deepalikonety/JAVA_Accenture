0793165128d8b1725435e4e87b9d2d3a860d6ea0
deepalikonety <deepalikonety@gmail.com>
2024-07-08 19:32:53 +0530

    first commit

---
+++ Demo1/src/Java1/DemoJava.java
@@ -0,0 +1,19 @@
+ /**
+ * 
+ */
+package Java1;
+
+/**
+ * 
+ */
+public class DemoJava {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
